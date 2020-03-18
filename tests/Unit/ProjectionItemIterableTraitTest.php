ab947899d8a693e0fc2699b0e058941267eb1a99
Hugo Gonçalves <hugo.goncalves@kununu.com>
2020-03-18 18:29:26 +0000

    Add unit test to ProjectionItemIterableTrait (#14)

---
+++ tests/Unit/ProjectionItemIterableTraitTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Kununu\Projections\Tests\Unit;
+
+use ArrayIterator;
+use BadMethodCallException;
+use Kununu\Projections\ProjectionItemIterable;
+use Kununu\Projections\ProjectionItemIterableTrait;
+use Kununu\Projections\Tag\Tags;
+use PHPUnit\Framework\TestCase;
+
+final class ProjectionItemIterableTraitTest extends TestCase
+{
+    public function testTrait(): void
+    {
+        $validClass = new class implements ProjectionItemIterable {
+            use ProjectionItemIterableTrait;
+
+            public function getKey(): string
+            {
+                return '';
+            }
+
+            public function getTags(): Tags
+            {
+                return new Tags();
+            }
+        };
+
+        $validClass->storeData([1, 2, 3]);
+
+        $this->assertEquals([1, 2, 3], $validClass->data());
+
+        $iterator = new ArrayIterator();
+        $iterator->append('a');
+        $iterator->append('b');
+        $iterator->append(5);
+        $validClass->storeData($iterator);
+
+        $this->assertEquals(['a', 'b', 5], $validClass->data());
+
+        $invalidClass = new class {
+            use ProjectionItemIterableTrait;
+        };
+
+        $this->expectException(BadMethodCallException::class);
+        $invalidClass->storeData([1, 2]);
+    }
+}
