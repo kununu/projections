4a1505948c02ece7ccab86b466e47011ea96e72b
Hugo Gonçalves <hugo.goncalves@kununu.com>
2026-05-21 16:27:14 +0100

    Bump GitHub Actions versions (#60)

--- rector-ci.php
+++ rector-ci.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitSelfCallRector;
-use Rector\PHPUnit\PHPUnit120\Rector\CallLike\CreateStubOverCreateMockArgRector;
-use Rector\PHPUnit\PHPUnit120\Rector\Class_\PropertyCreateMockToCreateStubRector;
 use Rector\Privatization\Rector\Class_\FinalizeTestCaseClassRector;
 
 return RectorConfig::configure()
@@ -19,11 +17,5 @@ return RectorConfig::configure()
     ->withSkip([
         __DIR__ . '/rector-ci.php',
         AddOverrideAttributeToOverriddenMethodsRector::class,
-        CreateStubOverCreateMockArgRector::class    => [
-            __DIR__ . '/src/TestCase/CacheCleaner/AbstractCacheCleanerTestCase.php',
-        ],
-        PropertyCreateMockToCreateStubRector::class => [
-            __DIR__ . '/tests/Provider/SimpleCachedProviderTestTraitTest.php',
-        ],
     ])
     ->withImportNames();
